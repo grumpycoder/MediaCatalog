d1890a4ca19557663fd9f314835e93cbeb0ecda0
Mark Lawrence <mark.lawrence@southernpoverty.onmicrosoft.com>
2017-01-23 12:16:04 -0600

    DataContext added

---
+++ MediaCatalog.DataAccess/MediaContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace MediaCatalog.DataAccess
+{
+    public class MediaContext : DbContext
+    {
+
+        public MediaContext() : base("name=MediaContext")
+        {
+
+        }
+    }
+}
